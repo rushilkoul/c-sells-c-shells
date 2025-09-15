8e8c453d3cce16fcc872e81b7a2edab2ce05374c
Rushil Koul <ruskoul3@gmail.com>
2025-09-15 23:54:13 +0530

    command handling + echo added

--- src/main.c
+++ src/main.c
@@ -21,33 +21,60 @@ void tokenize(char *s, char *argv[]) {
   argv[i] = NULL;
 }
 
+void echo(char *argv[]) {
+  int i = 1;
+
+  if (argv[1] == NULL) {
+    printf("command 'echo' expects at least (1) argument(s)\n");
+    return;
+  }
+
+  while(argv[i] != NULL) {
+      printf("%s", argv[i]);
+      if(argv[i + 1] != NULL)
+          printf(" ");
+      i++;
+  }
+  printf("\n");
+}
+
+void command(char *argv[]) {
+  char *com = argv[0];
+  
+  if(!strcmp(com, "exit")) 
+    exit(0);
+  else if(!strcmp(com, "echo"))
+    echo(argv);
+
+  else
+    printf("%s: command not found\n", com);
+}
+
 int repl() {
   printf("$ ");
-  char input[100];
-  fgets(input, 100, stdin);
+  char input[MAX_LINE];
+  fgets(input, MAX_LINE, stdin);
 
-  if (strlen(input) == 0) return 0;
+  if (input[0] == '\n') return 0;
 
   chomp(input);
 
   char *argv[MAX_ARGS];
-
   tokenize(input, argv);
+  /*
+  STRUCTURE:
+  args[0] contains the command name
+  args[1,2,3....] will contain the arguments 
+  at the respective positions 1, 2, 3...
+  */
 
-  printf("ARGS TEST:\n");
-  int test = 0;
-
-  while(argv[test] != NULL) {
-    printf("(%d): %s\n", test, argv[test]);
-    test++;
-  }
-
-  printf("%s: command not found\n", input);
+  command(argv);
   return 0;
 }
 int main() {
   setbuf(stdout, NULL);
   printf("\e[1;1H\e[2J"); // clear terminal
+  printf("C sells C shells - A (bad) shell written in C\n\n");
 
   while (1) {
     repl();
